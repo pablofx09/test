dc88f8c45397a0a680f7862bc323888caf2d554c
pablofx09 <148643828+pablofx09@users.noreply.github.com>
2023-10-22 14:01:32 -0500

    Update main.rs

--- main.rs
+++ main.rs
@@ -1,11 +1,19 @@
 fn main() {
-    let equation: &str = "7x = 35";
-    println!("{}", algebra(equation));
+    
+    let equation: &str = "4=3+45x";
+    //println!("{}", algebra(equation));
+    println!("{}", arrange(equation));
+    
+    
+    
 }
 //5x = 20
 fn algebra(eq: &str) -> String {
     let mut coef: i32 = 0;
     let mut constant: i32 = 0;
+    let mut left: i32 = 0;
+    let equals = finder(eq);
+    
     
     
     for i in 0..eq.len() {
@@ -13,22 +21,81 @@ fn algebra(eq: &str) -> String {
             if i == 0 {
                 coef = 1;
             }
-            else if &eq[i - 1..i] == " " {
-                coef = 1;
+            else if &eq[i - 1..i] == "-" {
+                coef = -1;
             }
             else {
-                coef = eq[i - 1..i].parse::<i32>().unwrap();
+                coef = eq[..i].parse::<i32>().unwrap();
+            }
+            if &eq[i + 1..i + 2] != "=" {
+                left = eq[i + 1..equals].parse::<i32>().unwrap();
             }
 
         }
         if &eq[i..i + 1] == "=" {
-            constant = eq[i + 2..].parse::<i32>().unwrap();
+            constant = eq[i + 1..].parse::<i32>().unwrap();
         }
     }
     let mut result: String = ("x = ").to_string();
+    constant -= left;
     result += &(format!("{}", constant/coef)).to_string();
+    
     
     return result;
 
   
 }
+
+fn finder(eq: &str) -> usize {
+    for i in 0..eq.len() {
+        if &eq[i..i + 1] == "=" {
+            return i;
+        }
+    }
+    return 0 as usize;
+}
+fn arrange(eq: &str) -> &str {
+    let mut temp = String::from("");
+    let _equals: usize = finder(eq);
+    for i in 0..eq.len() {
+    
+        if &eq[i..i + 1] == "x" {
+            if i == 0 {
+                temp += "x";
+            }
+            else if &eq[i - 1..i] == "+" || &eq[i - 1..i] == "=" {
+                temp += "x";
+            }
+            else if &eq[i - 1..i] == "-" {
+                temp += "-x";
+            }
+            else {
+                //start where the x is, loop through backwards until you get a +, -, =, or at the start of the string
+                //3+45x=9
+                let mut start: i32 = i as i32;
+                for j in (0..i).rev() {
+                    if j == 0 {
+                        start = 0;
+                    }
+                    else if &eq[j..j + 1] == "+" || &eq[j..j + 1] == "-" || &eq[j..j + 1] == "="  {
+                        break;
+                    }
+                    else {
+                        start -= 1;
+                    }
+                }
+                temp += &eq[start as usize..i];
+                temp += "x";
+                
+
+            }
+
+        }
+    
+    }
+    println!("{}", temp);
+    let _fin: &str = temp.as_str();
+    return "bob";
+}
+
+
